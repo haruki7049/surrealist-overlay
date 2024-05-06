ca2ee47962c00d8c61ca3c04e83115621adbd846
haruki7049 <tontonkirikiri@gmail.com>
2024-05-06 12:58:08 +0900

    chore: hoge

--- default.nix
+++ default.nix
@@ -1,14 +1,18 @@
 self: super: {
   surrealist = {
     "2.0.5" = let
+      inherit (super.stdenv.hostPlatform) system;
+      throwSystem = throw "This surrealist-overlay does not support system: ${system}";
     in super.appimageTools.wrapType2 rec {
       pname = "surrealist";
       version = "2.0.5";
 
-      src = super.fetchurl {
-        url = "https://github.com/surrealdb/surrealist/releases/download/surrealist-v${version}/surrealist_${version}_amd64.AppImage";
-        hash = "sha256-TRQZD+c76B2rGNBtkK1T2QTOzExUVyZOZmnehDRGL1I=";
-      };
+      src = {
+        x86_64-linux = super.fetchurl {
+          url = "https://github.com/surrealdb/surrealist/releases/download/surrealist-v${version}/surrealist_${version}_amd64.AppImage";
+          hash = "sha256-TRQZD+c76B2rGNBtkK1T2QTOzExUVyZOZmnehDRGL1I=";
+        };
+      }.${system} or throwSystem;
 
       meta = with super.lib; {
         description = "A scalable, distributed, collaborative, document-graph database, for the realtime web";
