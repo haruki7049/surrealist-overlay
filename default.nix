cf99779aa5fa84eecf432960a143177adfb196d2
haruki7049 <tontonkirikiri@gmail.com>
2024-05-06 12:53:17 +0900

    chore: Use AppImage

--- default.nix
+++ default.nix
@@ -1,16 +1,14 @@
 self: super: {
   surrealist = {
     "2.0.5" = let
-    in super.wrapType2 rec {
+    in super.appimageTools.wrapType2 rec {
       pname = "surrealist";
       version = "2.0.5";
 
       src = super.fetchurl {
-        url = "https://github.com/surrealdb/surrealist/releases/download/surrealist-v${version}/surrealist_${version}_amd64.AppImage.tar.gz";
-        hash = "";
+        url = "https://github.com/surrealdb/surrealist/releases/download/surrealist-v${version}/surrealist_${version}_amd64.AppImage";
+        hash = "sha256-TRQZD+c76B2rGNBtkK1T2QTOzExUVyZOZmnehDRGL1I=";
       };
-
-      sourceRoot = ".";
 
       meta = with super.lib; {
         description = "A scalable, distributed, collaborative, document-graph database, for the realtime web";
